db69356d46341822fc73ce16374bc00478a3c472
Oleksii Zezekalo <ars.ziko@gmail.com>
2024-06-20 23:04:03 +0200

    fixed build.gradle.kts issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,28 +17,14 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.zezekalo.iou"
-    compileSdk =
-        libs.versions.compile.sdk.version
-            .get()
-            .toInt()
+    compileSdk = libs.versions.compile.sdk.version.get().toInt()
 
     defaultConfig {
         applicationId = "com.zezekalo.iou"
-        minSdk =
-            libs.versions.min.sdk.version
-                .get()
-                .toInt()
-        targetSdk =
-            libs.versions.target.sdk.version
-                .get()
-                .toInt()
-        versionCode =
-            libs.versions.version.code
-                .get()
-                .toInt()
-        versionName =
-            libs.versions.version.name
-                .get()
+        minSdk = libs.versions.min.sdk.version.get().toInt()
+        targetSdk = libs.versions.target.sdk.version.get().toInt()
+        versionCode = libs.versions.version.code.get().toInt()
+        versionName = libs.versions.version.name.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -64,16 +50,12 @@ android {
         }
     }
     compileOptions {
-        val javaVersion =
-            libs.versions.jvm.target.version
-                .get()
+        val javaVersion = libs.versions.jvm.target.version.get()
         sourceCompatibility = JavaVersion.toVersion(javaVersion)
         targetCompatibility = JavaVersion.toVersion(javaVersion)
     }
     kotlinOptions {
-        jvmTarget =
-            libs.versions.jvm.target.version
-                .get()
+        jvmTarget = libs.versions.jvm.target.version.get()
     }
     buildFeatures {
         buildConfig = true
